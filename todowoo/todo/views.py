87aadd295f5ba3090bca00cfea85ab127957d72a
Artem Vitkovsky <workwith9512@gmail.com>
2023-07-13 14:07:34 +0300

    feature: add new functional for the registration form, fix errors

--- todowoo/todo/views.py
+++ todowoo/todo/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 
 def signupuser(request):
-    return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
+    else:
+        try:
+            if request.POST['password1'] == request.POST['password2']:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+        except IntegrityError:
+            return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'That name has alrady been taken'})
+
+        else:
+            return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'password mismatch'})
